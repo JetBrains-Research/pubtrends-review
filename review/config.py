40b1745c4ccac09dd2a24b3b0ac3fe5a61329394
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2022-08-30 17:59:05 +0200

    Working version of test_new_algo

--- review/config.py
+++ review/config.py
@@ -1,6 +1,7 @@
 # model structure
 d_hidden = 768  # 768
 d_bert = 768  # 768
+num_workers = 1
 
 # train
 amp_enabled = 1
@@ -12,6 +13,7 @@ seed = 1234
 base_path = "~/review"
 dataset_path = f"{base_path}/dataset"
 weights_path = f"{base_path}/weights"
+log_path = f"{base_path}/logs"
 
 # Model lookup is in /model and ~/.pubtrends/model folders
 model_name = 'learn_simple_berta.pth'
